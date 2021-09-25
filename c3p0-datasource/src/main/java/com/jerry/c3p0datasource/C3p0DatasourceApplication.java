b87495e8098eb85ed180a3f9563e4d278d4e2974
hanweida <jerryhan@sogou-inc.com>
2021-09-25 20:27:08 +0800

    Init

---
+++ c3p0-datasource/src/main/java/com/jerry/c3p0datasource/C3p0DatasourceApplication.java
@@ -0,0 +1,14 @@
+package com.jerry.c3p0datasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C3p0DatasourceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(C3p0DatasourceApplication.class, args);
+    }
+
+}
